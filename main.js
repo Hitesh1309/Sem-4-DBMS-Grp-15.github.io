a2a7908c514d03be7d50568ea162edd722d502be
Abishek Devendran <abishekdevendran@gmail.com>
2022-02-25 13:26:02 +0530

    Framework laid out

--- main.js
+++ main.js
@@ -4,6 +4,26 @@ function darkModeToggle(){
     element.classList.toggle("dark-mode");
 }
 
-//Binding dark mode toggle to button
-let dToggle=document.getElementById("dark-mode-toggler");
-dToggle.onclick=darkModeToggle;
+//Funtion to control top button visibility
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    Upbutton.style.display = "block";
+  } else {
+    Upbutton.style.display = "none";
+  }
+}
+
+//Function to scroll up
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//Binding scrolling function to button
+let Upbutton = document.getElementById("myBtn");
+let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+window.onscroll = function () {
+  if(width>600){
+    scrollFunction();
+  }
+};
